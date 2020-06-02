f4abb3eedde625e3e7d95e6ff0f89018a0faf6cc
EC2 Default User <ec2-user@ip-172-31-37-56.ap-northeast-1.compute.internal>
2020-06-02 15:11:05 +0000

    Finish editing-users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 User.create(name: "Sample User",
             email: "sample@email.com",
             password: "password",
-            password_confirmation: "password")
+            password_confirmation: "password",
+            admin: true)
             
 60.times do |n|
   name = Faker::Name.name
