87c9a3b4211c33276b94e943f61597f049478568
toshiyuki-cpu <spd36bq9@gmail.com>
2020-06-17 17:39:18 +0000

    show.html,15分単位実装

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -11,6 +11,12 @@ module AttendancesHelper
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+  #def format_hour(time)
+   # format("%.2f", (time.hour))
+  #end
+  
+  #def started_at
+    #Time.current.floor_to(15.minutes)
+  #end
 end
-
-
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,5 @@
 class Attendance < ApplicationRecord
+  
   belongs_to :user
   
   validates :worked_on, presence: true
